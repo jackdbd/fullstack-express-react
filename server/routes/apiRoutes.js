3f6c86056936214f3ca73ca9aa15a3b1749e5c5d
jackdbd <jackdebidda@gmail.com>
2018-05-25 20:10:39 +0200

    Define REST endpoints in server/routes

--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -2,8 +2,39 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/test", (req, res) => {
-  res.send({ "test-key": "This is a test" });
+router.post("/signup", (req, res) => {
+  res.send({ TODO: "Sign​ ​up​ ​to​ ​the​ ​system​ ​(username,​ ​password)" });
+});
+
+router.post("/login", (req, res) => {
+  res.send({ TODO: "Logs​ ​in​ ​an​ ​existing​ ​user​ ​with​ ​a​ ​password" });
+});
+
+router.get("/me", (req, res) => {
+  res.send({
+    TODO: "AUTH Get​ ​the​ ​currently​ ​logged​ ​in​ ​user​ ​information"
+  });
+});
+
+// I think update password should be idempotent, so PUT is better than POST
+router.put("/me/update-password", (req, res) => {
+  res.send({ TODO: "AUTH Update​ ​the​ ​current​ ​user's​ ​password" });
+});
+
+router.get("/user/:id/", (req, res) => {
+  res.send({
+    TODO: "List​ ​username​ ​&​ ​number​ ​of​ ​likes​ ​of​ ​a​ ​user"
+  });
+});
+
+router.get("/user/:id/like", (req, res) => {
+  res.send({ TODO: "AUTH Like a user" });
+});
+
+router.get("/most-liked", (req, res) => {
+  res.send({
+    TODO: "List​ ​users​ ​in​ ​a​ ​most​ ​liked​ ​to​ ​least​ ​liked"
+  });
 });
 
 module.exports = router;
