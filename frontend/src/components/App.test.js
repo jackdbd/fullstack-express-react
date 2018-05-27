2d4ca80289d50c4fdfe9fdc969996aa22174cd41
jackdbd <jackdebidda@gmail.com>
2018-05-27 08:31:42 +0200

    Reorganize project structure
    - Move App component to /components
    - Remove unnecessary files

---
+++ frontend/src/components/App.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render, unmountComponentAtNode } from 'react-dom';
+import App from './App';
+
+describe("App component", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    render(
+        <App />,
+      div
+    );
+    unmountComponentAtNode(div);
+  });
+})
